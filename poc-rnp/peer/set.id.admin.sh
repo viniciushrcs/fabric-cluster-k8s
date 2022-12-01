7c014e83ad7f150472f29f312371aee375bbf4d0
viniciushrcs <viniciushrcs@gmail.com>
2022-12-01 06:39:17 -0300

    peer updates and set admin id script

--- poc-rnp/peer/set.id.admin.sh
+++ poc-rnp/peer/set.id.admin.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export CORE_PEER_MSPCONFIGPATH=/home/viniciushrcs/vini_projects/fabric-cluster-k8s/poc-rnp/crypto-config/peerOrganizations/rnp.com/users/Admin@rnp.com/msp
 
 echo ' Switched identity to Peer Admin!!'
