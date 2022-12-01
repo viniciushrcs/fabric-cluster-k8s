efac05df6b03841659699a77c937ead4f49663b3
viniciushrcs <viniciushrcs@gmail.com>
2022-12-01 06:08:18 -0300

    add peer configs

---
+++ poc-rnp/peer/set.id.admin.sh
@@ -0,0 +1,3 @@
+export CORE_PEER_MSPCONFIGPATH=/home/viniciushrcs/vini_projects/fabric-cluster-k8s/poc-rnp/crypto-config/peerOrganizations/rnp.com/users/Admin@rnp.com/msp
+
+echo ' Switched identity to Peer Admin!!'
